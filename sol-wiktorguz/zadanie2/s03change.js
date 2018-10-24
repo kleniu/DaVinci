1069559e365a9a2619fad36e3cb527aa4d7d0c37
Wiktor Guź <9108400+wiktorguz@users.noreply.github.com>
2018-10-24 20:47:13 +0200

    Add solution for tasks 1 and 2

---
+++ sol-wiktorguz/zadanie2/s03change.js
@@ -0,0 +1,37 @@
+var hfcWorker = require('./util/hfcWorker');
+
+var ccFunKeyValue = {
+    fcn: 'changeAsset',
+    key: 'asset1',
+    val: { value:0, color:'grey', owner:'Tom' }
+};
+
+hfcWorker.doInvoke(
+    function(transaction_id,chaincode_id,targets,channel_id){
+        return {
+            targets: targets,
+            chaincodeId: chaincode_id,
+            fcn: ccFunKeyValue.fcn,
+            args: [ccFunKeyValue.key, JSON.stringify(ccFunKeyValue.val)],
+            chainId: channel_id,
+            txId: transaction_id
+        }
+    }
+    ,
+    function(invoke_results){
+	if (invoke_results[0] instanceof Error) {
+            console.error("error from invoke = ", invoke_results[0]);
+        }
+	else {
+            console.log('function : "'+ccFunKeyValue.fcn+'"');
+            console.log('key      : "'+ccFunKeyValue.key+'"');
+            console.log('val      :');
+            console.log(JSON.stringify(ccFunKeyValue.val, null, 4));
+            console.log('response :');
+            console.log(JSON.stringify(invoke_results[0], null, 4));
+	}
+	return invoke_results[0];	
+    }
+).catch((err)=>{
+    console.log("worker error="+err);
+});
---
+++ sol-wiktorguz/zadanie2/s05delete.js
@@ -0,0 +1,34 @@
+var hfcWorker = require('./util/hfcWorker');
+
+var ccFunKey = {
+    fcn: 'deleteAsset',
+    key: 'asset1'
+};
+
+hfcWorker.doInvoke(
+    function (transaction_id, chaincode_id, targets, channel_id) {
+        return {
+            targets: targets,
+            chaincodeId: chaincode_id,
+            fcn: ccFunKey.fcn,
+            args: [ccFunKey.key],
+            chainId: channel_id,
+            txId: transaction_id
+        }
+    }
+    , 
+    function (invoke_results) {
+	if (invoke_results[0] instanceof Error) {
+            console.error("error from invoke = ", invoke_results[0]);
+        }
+	else {
+            console.log('function : "' + ccFunKey.fcn + '"');
+            console.log('key      : "' + ccFunKey.key + '"');
+            console.log('response :');
+            console.log(JSON.stringify(invoke_results[0], null, 4));
+        }
+        return invoke_results[0]; // the first returned value is from the 'sendPromise' which is from the 'sendTransaction()' call
+    }
+).catch((err)=>{
+    console.error("worker error="+err);
+});
